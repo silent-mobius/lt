4a28d6bf1876463bd1856046c1479bbc00e2ead1
sient-moius <alex@vaiolabs.com>
2020-12-30 18:55:00 +0200

    adding gitignore on function_test branch

---
+++ lib/tests/function_test.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/env bats
+#######################################################
+#
+#
+#
+#
+#######################################################
+
+#sourcing the make script
+. ./make.sh
+
+#####################
+# List of functions #
+#####################
+# set_xsl
+# set_java
+# check_rootdir
+# add_mod
+# clean
+# check_book
+# build_header
+# build_footer
+# build_part_body
+# fill_part
+# build_part
+# build_body
+# build_xml
+# build_pdf
+# build_html
+# check_os_type
+
+
+@test "Test: set_xsl" {
+
+}
+
+@test "Test: set_java" {
+
+}
+
+@test "Test: check_rootdir" {
+
+}
+
+@test "Test: add_mod" {
+
+}
+
+@test "Test: clean" {
+
+}
+
+@test "Test: check_book " {
+
+}
+
+@test "Test: check_header" {
+
+}
+
+@test "Test: check_footer " {
+
+}
+
+@test "Test: build_part_body " {
+
+}
+
+@test "Test: fill_part" {
+
+}
+
+@test "Test:  build_part" {
+
+}
+@test "Test: build_body " {
+
+}
+@test "Test: build_xml" {
+
+}
+@test "Test: build_pdf" {
+
+}
+@test "Test: build_html" {
+
+}
+@test "Test: check_os_type" {
+
+}
